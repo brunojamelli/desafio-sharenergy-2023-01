08594f741fbd113dc344678b5b278ba748a720cd
brunojamelli <brunojamelli@ufrn.edu.br>
2023-01-06 12:43:09 -0300

    feat: adding basic api structure and installing libs

--- api-rest/src/routes.js
+++ api-rest/src/routes.js
@@ -1,8 +1,11 @@
-const UserController = require('./controllers/UserController');
+const ClientController = require('./controllers/ClientController');
 
 const express = require('express');
 const routes = express.Router();
 // rotas de autenticação
-routes.post('/user', UserController.post);
-routes.get('/user', UserController.get);
+routes.post('/client', ClientController.post);
+routes.get('/client', ClientController.get);
+routes.put('/client/:id', ClientController.put);
+routes.delete('/client/:id', ClientController.delete);
+routes.get('/client/:id', ClientController.getById);
 module.exports = routes;
