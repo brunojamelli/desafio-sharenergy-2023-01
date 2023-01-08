11dafb2c97080bd38fe84cf6e38db240bbe7f110
brunojamelli <brunojamelli@ufrn.edu.br>
2023-01-07 23:02:37 -0300

    feat: changing the logs of actions of client routes to the pino library

--- api-rest/src/controllers/ClientController.ts
+++ api-rest/src/controllers/ClientController.ts
@@ -4,29 +4,28 @@ import Client from "./../models/client";
 
 export default {
    async create(req: Request, res: Response) {
-      logger.info("pooost");
-      const client = new Client({
-         name: 'Bill',
-         email: 'bill@initech.com'
-      });
-      await client.save();
-
-      console.log(client.email);
-      res.status(201).send(client);
+      const { name, email, phoneNumber, cpf } = req.body;
+      const newClient = new Client({
+         name, email, phoneNumber, cpf
+      })
+      await newClient.save();
+      logger.info(`client saved \n ${newClient}`)
+      res.status(201).send(newClient);
    },
 
    async update(req: Request, res: Response) {
       const { name, email, phoneNumber, cpf } = req.body;
       logger.info(req.params.id);
       const updatedClient = await Client.findByIdAndUpdate(req.params.id, { name, email, phoneNumber, cpf }, { new: true });
+      logger.info(`client updated \n ${updatedClient}`)
       res.send(updatedClient);
-      // res.status(201).send(`Rota PUT com ID! --> ${id}`);
    },
 
    async delete(req: Request, res: Response) {
       let id = req.params.id;
-      const client = await Client.findByIdAndDelete(id);
-      res.status(200).send(client);
+      const erasedClient = await Client.findByIdAndDelete(id);
+      logger.info(`client erased \n ${erasedClient}`)
+      res.status(200).send(erasedClient);
    },
 
    async show(req: Request, res: Response) {
@@ -36,8 +35,8 @@ export default {
 
    async index(req: Request, res: Response) {
       let idClient = req.params.id;
-      logger.info(idClient);
-      let client = await Client.findById(idClient).exec();
-      res.status(200).send(`cliente ${idClient} \n com os dados:\n ${client}`);
+      let searchedClient = await Client.findById(idClient).exec();
+      logger.info(`searched client \n ${searchedClient}`);
+      res.status(200).send(`cliente ${idClient} \n com os dados:\n ${searchedClient}`);
    }
 }
--- api-rest/src/database.ts
+++ api-rest/src/database.ts
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import { logger } from './logger';
 
 // Connect to the MongoDB database
+mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost:27017/local', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 // Get the connection
@@ -11,23 +13,23 @@ const connection = mongoose.connection;
 
 // Set up the connection event listeners
 connection.on('connected', () => {
-  console.log('Mongoose connected');
+    logger.info('mongoose connected');
 });
 
 connection.on('error', (err: any) => {
-  console.log(`Mongoose connection error: ${err}`);
+    console.log(`Mongoose connection error: ${err}`);
 });
 
 connection.on('disconnected', () => {
-  console.log('Mongoose disconnected');
+    console.log('Mongoose disconnected');
 });
 
 // If the Node process ends, close the Mongoose connection
 process.on('SIGINT', () => {
-  connection.close(() => {
-    console.log('Mongoose connection disconnected through app termination');
-    process.exit(0);
-  });
+    connection.close(() => {
+        console.log('Mongoose connection disconnected through app termination');
+        process.exit(0);
+    });
 });
 
 export { connection };
