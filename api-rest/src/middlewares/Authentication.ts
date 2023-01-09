cc7faa81c6752055b541987db68307efead2afa2
brunojamelli <brunojamelli@ufrn.edu.br>
2023-01-09 12:49:10 -0300

    feat: adding authentication middleware and authenticator controller

---
+++ api-rest/src/middlewares/Authentication.ts
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+// Create an interface for the Request object with a user property
+interface AuthenticatedRequest extends Request {
+    user: object;
+}
+export default {
+    auth(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+        // Get the JWT from the request header
+        const token = req.headers.authorization;
+        const secret = process.env.SECRET || '';
+
+        if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+
+        jwt.verify(token, secret, function (err, decoded) {
+            if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+
+            // se tudo estiver ok, salva no request para uso posterior
+            req.user = decoded as object;
+            next();
+        });
+    }
+}
--- api-rest/src/routes.ts
+++ api-rest/src/routes.ts
@@ -1,33 +1,37 @@
 import ClientController from "./controllers/ClientController";
-import { Router } from "express";
+import UserController from "./controllers/UserController";
+import Authentication from "./middlewares/Authentication";
+import { Router, Request, Response } from "express";
 const routes = Router();
-import jwt from 'jsonwebtoken';
+// const auth: Authentication = new Authentication();
 
-// rotas de autenticação
-routes.post('/client', ClientController.create);
+// rota de autenticação
+routes.post('/login', UserController.login);
+routes.get('/home', (req: Request, res: Response) => { return res.json("bem vindo") });
+routes.post('/client', Authentication.auth, ClientController.create);
 routes.get('/client', ClientController.show);
 routes.put('/client/:id', ClientController.update);
 routes.delete('/client/:id', ClientController.delete);
 routes.get('/client/:id', ClientController.index);
 
-routes.post('/login', (req, res) => {
-    // Check the username and password
-    if (req.body.username === 'desafiosharenergy' && req.body.password === 'sh@r3n3rgy') {
-        // If the username and password are correct, generate a JWT
-        const token = jwt.sign({
-            user: 'desafiosharenergy'
-        }, process.env.SECRET_KEY || "secret");
+// routes.post('/login', (req, res) => {
+//     // Check the username and password
+//     if (req.body.username === 'desafiosharenergy' && req.body.password === 'sh@r3n3rgy') {
+//         // If the username and password are correct, generate a JWT
+//         const token = jwt.sign({
+//             user: 'desafiosharenergy'
+//         }, process.env.SECRET_KEY || "secret");
 
-        // Return the JWT to the client
-        res.json({
-            token
-        });
-    } else {
-        // If the username or password is incorrect, return an error
-        res.status(401).json({
-            error: 'Invalid username or password'
-        });
-    }
-});
+//         // Return the JWT to the client
+//         res.json({
+//             token
+//         });
+//     } else {
+//         // If the username or password is incorrect, return an error
+//         res.status(401).json({
+//             error: 'Invalid username or password'
+//         });
+//     }
+// });
 
 export default routes;
