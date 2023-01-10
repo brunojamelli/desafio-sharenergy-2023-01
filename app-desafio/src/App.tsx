8f1c5ca0f4d98ba0f5cbfc24004b5a3aaca3c319
brunojamelli <brunojamelli@ufrn.edu.br>
2023-01-10 11:42:16 -0300

    feat: adding sidebar structure and react router

--- app-desafio/src/App.tsx
+++ app-desafio/src/App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>a</p>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <p>a</p>
+    //   </header>
+    // </div>
+    <>
+        <Sidebar />
+    </>
   );
 }
 
