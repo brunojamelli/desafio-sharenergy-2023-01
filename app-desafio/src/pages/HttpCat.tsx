dfb9155393812ae894d071c0126b18b78f2b2b39
brunojamelli <brunojamelli@ufrn.edu.br>
2023-01-10 21:09:02 -0300

    feat: adding httpcat page

--- app-desafio/src/pages/HttpCat.tsx
+++ app-desafio/src/pages/HttpCat.tsx
@@ -1,19 +1,50 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const HttpCat: React.FunctionComponent = () => {
-    const [cat, setCat] = useState<[]>([]);
-    useEffect(() => {
-        axios.get('https://randomuser.me/api/?results=10')
-            .then(res => {
-                setCat(res.data.results)
-            })
-    }, [])
-    return (
-        <div>
-            
-        </div>
-    )
+interface HttpCatImage {
+    file: string;
 }
 
-export default HttpCat
+
+const HttpCat: React.FC = () => {
+    const [statusCode, setStatusCode] = useState<string>('');
+    const [httpCatImage, setHttpCatImage] = useState<HttpCatImage | null>(null);
+  
+    useEffect(() => {
+      const fetchImage = async () => {
+        const response = await fetch(`https://http.cat/${statusCode}`);
+        if (!response.ok) {
+          // error handling
+          throw new Error('Failed to load image');
+        }
+        const image = await response.json();
+        setHttpCatImage(image);
+      };
+      if (statusCode) {
+        fetchImage();
+      }
+    }, [statusCode]);
+  
+    return (
+      <div>
+        <h1>HTTP Cat Image Finder</h1>
+        <div>
+          <label htmlFor="status-code">Status Code:</label>
+          <input
+            type="number"
+            name="status-code"
+            value={statusCode}
+            onChange={(event) => setStatusCode(event.target.value)}
+          />
+        </div>
+        {httpCatImage && (
+          <div>
+            <h2>Image:</h2>
+            <img src={httpCatImage.file} alt="HTTP Cat Image" />
+          </div>
+        )}
+      </div>
+    );
+  };
+
+export default HttpCat;
